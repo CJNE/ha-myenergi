26678277a8deb09158fc97a86dfac22a07586a99
Rob Ferrer <rob@ferrer.co.uk>
2023-11-04 15:09:17 +0000

    Add stop boost service
    Closes #172

--- custom_components/myenergi/entity.py
+++ custom_components/myenergi/entity.py
@@ -64,6 +64,12 @@ class MyenergiEntity(CoordinatorEntity):
         await self.device.start_smart_boost(amount, when)
         self.schedule_update_ha_state()
 
+    async def stop_boost(self) -> None:
+        _LOGGER.debug("Stop boost called")
+        """Stop boost"""
+        await self.device.stop_boost()
+        self.schedule_update_ha_state()
+
 
 class MyenergiHub(CoordinatorEntity):
     def __init__(self, coordinator, config_entry, meta):
