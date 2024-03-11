26ad47170c8cfff0a1321b055b870658aaa72640
Johnny Willemsen <jwillemsen@remedy.nl>
2024-03-11 12:14:54 +0100

    Add sensor for number of phases but that doesn't seem to be supported yet on the Zappi     * custom_components/myenergi/sensor.py:

--- custom_components/myenergi/sensor.py
+++ custom_components/myenergi/sensor.py
@@ -310,6 +310,14 @@ async def async_setup_entry(hass, entry, async_add_devices):
                     coordinator,
                     device,
                     entry,
+                    create_meta("Number of phases", "num_phases", ENTITY_CATEGORY_DIAGNOSTIC),
+                )
+            )
+            sensors.append(
+                MyenergiSensor(
+                    coordinator,
+                    device,
+                    entry,
                     create_energy_meta(
                         "Energy used today", "energy_total", ENTITY_CATEGORY_DIAGNOSTIC
                     ),
