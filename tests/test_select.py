f3942c726d2aa44762c9622e5a0e660a81262739
Johnny Willemsen <jwillemsen@remedy.nl>
2024-06-03 13:57:06 +0200

    Test phase setting
        * tests/test_select.py:

--- tests/test_select.py
+++ tests/test_select.py
@@ -11,10 +11,34 @@ from homeassistant.core import HomeAssistant
 from . import setup_mock_myenergi_config_entry
 
 TEST_ZAPPI_SELECT_CHARGE_MODE = "select.myenergi_test_zappi_1_charge_mode"
+TEST_ZAPPI_SELECT_PHASE_SETTING = "select.myenergi_test_zappi_1_phase_setting_select"
 TEST_EDDI_SELECT_OP_MODE = "select.myenergi_test_eddi_1_operating_mode"
 
 
 async def test_zappi_select(
+    hass: HomeAssistant, mock_zappi_set_phase_setting: MagicMock
+) -> None:
+    """Verify device information includes expected details."""
+
+    await setup_mock_myenergi_config_entry(hass)
+
+    await hass.services.async_call(
+        SELECT_DOMAIN,
+        SERVICE_SELECT_OPTION,
+        {
+            ATTR_ENTITY_ID: TEST_ZAPPI_SELECT_PHASE_SETTING,
+            ATTR_OPTION: "1",
+        },
+        blocking=False,
+    )
+    assert mock_zappi_set_phase_setting.call_count == 0
+    await hass.async_block_till_done()
+    assert mock_zappi_set_phase_setting.call_count == 1
+    mock_zappi_set_phase_setting.assert_called_with("1")
+
+
+
+async def test_zappi_phaseselect(
     hass: HomeAssistant, mock_zappi_set_charge_mode: MagicMock
 ) -> None:
     """Verify device information includes expected details."""
