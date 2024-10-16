0fcf99c924f332b0d94ea7df78da62c277c4f2f6
Johan Isacsson <johan@generatorhallen.se>
2024-10-16 22:30:35 +0200

    Version 0.0.29

--- custom_components/myenergi/const.py
+++ custom_components/myenergi/const.py
@@ -4,7 +4,7 @@
 NAME = "myenergi"
 DOMAIN = "myenergi"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.0.28"
+VERSION = "0.0.29"
 
 ATTRIBUTION = "Data provided by myenergi"
 ISSUE_URL = "https://github.com/CJNE/ha-myenergi/issues"
